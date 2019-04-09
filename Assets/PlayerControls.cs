d8ce8df7a2f269f6840e05fe7cb73b6e98b372f3
Basil Gerard <nicolas1gerard@hotmail.fr>
2019-04-09 17:07:23 +0200

    ground work
    Structure of the system, camera relative input reading

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    //references
+    private CharacterController controller;
+    private Transform m_Camera;
+    private Vector3 m_CameraForward;
+    private Vector3 m_lStickInputVector; //camera relative
+    //internal state
+    bool m_isJumping = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponentInChildren<CharacterController>();
+        m_Camera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_isJumping)
+        {
+            //jump code in there to ensure no input is missed
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //adapted from some of the standard unity asset code
+        float h_lstick = Input.GetAxis("Horizontal");
+        float v_lstick = Input.GetAxis("Vertical");
+
+        //camera relative input
+        if(m_Camera != null)
+        {
+            m_CameraForward = Vector3.Scale(m_Camera.forward, new Vector3(1, 0, 1)).normalized;
+            m_lStickInputVector = h_lstick * m_Camera.right + v_lstick * m_CameraForward;
+
+            //pass the input vector to the stuff that caresabout it
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    public float CharacterSpeed = 20.0f;
+    public float steeringSpeed = 1.0f;
+
+    enum controlState//the animator should ultimately removethe need for this,I put it there just in case it is needed
+    {
+        walking,
+        running,
+        sprinting,
+        airborne,
+        falling
+    }
+
+    private void Awake()
+    {
+        controller = GetComponentInChildren<CharacterController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        //transform.rotation = 
+
+
+
+    }
+
+    private void Jump()
+    {
+        Debug.Log("Jump!");
+    }
+
+
+
+}
