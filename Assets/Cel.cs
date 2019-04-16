bfb77bcd6464d5ff1f5d58d05731cfd3c495ec15
Isak Mansén <roostatbrood@gmail.com>
2019-04-16 11:26:06 +0200

    shader testing

---
+++ Assets/Cel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cel : MonoBehaviour
+{
+    private Light light = null;
+
+    private void OnEnable()
+    {
+        light = this.GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        shader.SetGlobalVector("_CelDirection", -this.transform.forward);
+    }
+}
