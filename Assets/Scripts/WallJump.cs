00db77a83bb177b502243f75c0e4b2ff35389273
Basil Gerard <nicolas1gerard@hotmail.fr>
2019-04-25 13:56:46 +0200

    Wall detection

--- Assets/Scripts/WallJump.cs
+++ Assets/Scripts/WallJump.cs
@@ -10,45 +10,87 @@ public class WallJump : MonoBehaviour
     //this is a temporary implementation
     PlayerMovement m_movementScript;
     CharacterController m_controlller;
-    [Tooltip("Minimal height around the player's transform the wall needs to cover for them to stick to it")]
-    [SerializeField] private float m_minimalWallHeightFactor = 0.5f;
-    private float m_reach;//how far the raycasts will "feel" for the wall, higher values will allow to wall run on walls of different incline.
+    [Tooltip("Distance beyond the player's collider the game will 'feel' for walls")]
+    [SerializeField] private float m_reach = 0.5f;
+    private float m_absoluteReach;
+
+    //allocations
+    private float m_shortestHitDistance;//used in choosing which ray determines the angle of collision with walls
+    private RaycastHit m_hit;//allocated memory to read raycast hits
+    private Direction m_direction;
+    private Vector3 m_origin;  //origin of the rays that detect walls
+    enum Direction
+    {
+        None,
+        Front,
+        Left,
+        Right,
+    }
+
     private void Awake()
     {
         m_movementScript = GetComponent<PlayerMovement>();
         m_controlller = GetComponent<CharacterController>();
-        m_controlller.radius
+        m_absoluteReach = m_controlller.radius + m_controlller.skinWidth + m_reach;
+
     }
 
-    private void OnControllerColliderHit(ControllerColliderHit hit)
+    private void FixedUpdate()
     {
-        //check if in falling or jumping state
-        //check if the collisionis with a wall, in this case, any vertical surface(with some leeway)
-        Vector3 direction = hit.point - transform.position;
-        direction = new Vector3(direction.x, 0.0f, direction.z);
-        float angle = Vector3.SignedAngle(-direction, hit.normal, Vector3.up);
-        if(direction.magnitude > m_controlller.radius){
-            if (Physics.Raycast(transform.position + Vector3.up * m_minimalWallHeightFactor * m_controlller.height, direction, m_reach) &&
-            Physics.Raycast(transform.position - Vector3.up * m_minimalWallHeightFactor * m_controlller.height, direction, m_reach))
-                InitiateWallRun(angle);
-        }
-        
-        
-    }
-
-    private void InitiateWallRun(float angle)
-    {
-        if(angle > 20.0f)
+        //check for falling/jumping state
+        if (detectWalls())
         {
-            Debug.Log("wall on left");
-
-        }else if(angle < -20.0f)
-        {
-            Debug.Log("wallon right");
+            InitiateWallRun();
         }
         else
         {
-            Debug.Log("Frontal collision");
+            Debug.Log(m_shortestHitDistance);
+        }
+
+    }
+
+    private bool detectWalls()
+    {
+        m_shortestHitDistance = m_absoluteReach + 1.0f;//initialized here so any hit will be shorter
+        m_direction = Direction.None;
+        m_origin = transform.position + new Vector3(0.0f, m_controlller.height / 2.0f, 0.0f);
+        CheckDirection(new Ray(m_origin, transform.forward), Direction.Front);
+        Debug.DrawRay(m_origin, transform.forward, Color.green);
+        CheckDirection(new Ray(m_origin, transform.right), Direction.Right);
+        Debug.DrawRay(m_origin, transform.right, Color.green);
+        CheckDirection(new Ray(m_origin, -transform.right), Direction.Left);
+        Debug.DrawRay(m_origin, transform.right, Color.green);
+        return (m_direction != Direction.None);
+    }
+
+    private void CheckDirection(Ray ray, Direction direction)
+    {
+        if (Physics.Raycast(ray, out m_hit, m_absoluteReach) && m_hit.distance < m_shortestHitDistance)
+        {
+            m_shortestHitDistance = m_hit.distance;
+            m_direction = direction;
+        }
+    }
+    
+
+    private void InitiateWallRun()
+    {
+        switch (m_direction)
+        {
+            case Direction.None:
+                Debug.LogError("Invalid walljump direction, value has been modified at an unexpected point");
+                break;
+            case Direction.Front:
+                Debug.Log("wall in front");
+                break;
+            case Direction.Left:
+                Debug.Log("Wall on the left");
+                break;
+            case Direction.Right:
+                Debug.Log("Wall on the right");
+                break;
+            default:
+                break;
         }
     }
 }
