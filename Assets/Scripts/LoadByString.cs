9aaaf626ca3661d65979f04214ddb39a28014a22
vororo <adamolsson@outlook.com>
2019-04-26 10:00:14 +0200

    Fixed the loadScene stackoverflow.
    I called my own loadScene infinitely instead of Unitys LoadScene.

--- Assets/Scripts/LoadByString.cs
+++ Assets/Scripts/LoadByString.cs
@@ -1,12 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadByString : MonoBehaviour
 {
     
     public void loadScene(string SceneName)
     {
-        loadScene(SceneName);
+        StartCoroutine("LoadYourAsyncScene", SceneName);
     }
+
+    IEnumerator LoadYourAsyncScene(string SceneName)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
 }
