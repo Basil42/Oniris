59850277318fc7b68d4e1f54bccdcd1a1ae22e63
vororo <adamolsson@outlook.com>
2019-04-30 15:19:56 +0200

    Dialogue Improvements and a findPlayer script for the freelook cameras
    Dialogue is now timed and the image also transitions into view with the text

--- Assets/Scripts/DialougeSystem.cs
+++ Assets/Scripts/DialougeSystem.cs
@@ -9,13 +9,15 @@ using TMPro;
 //It should reveal the character image on a trigger
 //It should write in text in the text box
 
+    //TODO: improve lerp, possibly do another way
 public class DialougeSystem : MonoBehaviour
 {
     public Image image;
     public TextMeshProUGUI textBubble;
     private float targetPosition;
     private float dialogueStep = 0.01f;
-    private bool endDialogue = false;
+    private float imageStep = 0.01f;
+    private bool runningDialogue = false;
 
     private void Start()
     {
@@ -27,47 +29,54 @@ public class DialougeSystem : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.A)) //Placeholder
         {
-            StartDialogue("Hello There");
+            StartDialogue("Hello There",5);
         }
     }
 
+    public void StartDialogue(string dialogue, float time)
+    {
+        if (!runningDialogue)
+        {
+            runningDialogue = true;
+            textBubble.text = dialogue;
+            StartCoroutine(ShowImage());
+            StartCoroutine("RunDialogue");
+            Invoke("StopDialogue", time);
+        }
+    }
+
+    //Moves image into view as long as dialogue is running, then starts HideImage
     public IEnumerator ShowImage()
     {
-        //Lerp the image into view?
-        //Mathf.Lerp(CurrentPosition , targetPosition, step);
-        while(image.fillAmount < 1)
+        imageStep = 0.01f;
+        while (runningDialogue)
         {
-            image.fillAmount += 0.01f;
+           // image.fillAmount += 0.01f;
+            image.transform.localPosition = new Vector3(Mathf.Lerp(image.transform.localPosition.x, -360, imageStep), image.transform.localPosition.y, image.transform.localPosition.z);
+            imageStep += 0.001f;
             yield return new WaitForFixedUpdate();
         }
         StartCoroutine(HideImage());
     }
 
+    //While not running, move image out of view, end after a little while or when a new dialogue starts running
     public IEnumerator HideImage()
     {
-        //Lerp the image out of view?
-        while (image.fillAmount > 0)
+        imageStep = 0.01f;
+        while (imageStep < 0.3f && !runningDialogue)
         {
-            image.fillAmount -= 0.01f;
+            //image.fillAmount -= 0.01f;
+            image.transform.localPosition = new Vector3(Mathf.Lerp(image.transform.localPosition.x, -200, imageStep), image.transform.localPosition.y, image.transform.localPosition.z);
+            imageStep += 0.001f;
             yield return new WaitForFixedUpdate();
         }
-        endDialogue = true;
     }
 
-    public void StartDialogue(string dialogue)
+    //Moves dialogue into view, also transitions into EndDialogue when no longer runningDialogue is true
+    private IEnumerator RunDialogue()
     {
-        endDialogue = false;
-        textBubble.text = dialogue;
-        StartCoroutine(ShowImage());
-        StartCoroutine("RunDialogue", "Hello There");
-        //IEnumerate over the dialouge and update it over time?
-    }
-
-    private IEnumerator RunDialogue(string dialouge)
-    {
-        // GetComponentInChildren<RectTransform>().rect.position;
         dialogueStep = 0.01f;
-        while (!endDialogue/*textBubble.transform.localPosition.x > targetPosition*/)
+        while (runningDialogue)
         {
             textBubble.transform.localPosition = new Vector3(Mathf.Lerp(textBubble.transform.localPosition.x, targetPosition, dialogueStep), textBubble.transform.localPosition.y, textBubble.transform.localPosition.z);
             dialogueStep += 0.001f;
@@ -76,16 +85,22 @@ public class DialougeSystem : MonoBehaviour
         StartCoroutine(EndDialogue());
     }
 
+    //While not running, move text out of view, end after a little while or when a new dialogue starts running
     private IEnumerator EndDialogue()
     {
         print("EndingDialogue");
         dialogueStep = 0.01f;
-        while (dialogueStep < 0.3f && endDialogue/*textBubble.transform.localPosition.x < targetPosition*/)
+        while (dialogueStep < 0.3f && !runningDialogue)
         {
             print(dialogueStep);
             textBubble.transform.localPosition = new Vector3(Mathf.Lerp(textBubble.transform.localPosition.x, -200, dialogueStep), textBubble.transform.localPosition.y, textBubble.transform.localPosition.z);
             dialogueStep += 0.001f;
             yield return new WaitForFixedUpdate();
         }
+    }
+
+    private void StopDialogue() //Meant to be invoked as a timer for the dialogue
+    {
+        runningDialogue = false;
     }
 }
