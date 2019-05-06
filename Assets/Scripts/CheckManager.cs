820eac1e1a830faea76ba6f2980becb20ba05242
Basil Gerard <nicolas1gerard@hotmail.fr>
2019-05-06 15:03:09 +0200

    passing array

--- Assets/Scripts/CheckManager.cs
+++ Assets/Scripts/CheckManager.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Runtime.InteropServices;
-
 
 public enum checkContent
 {
@@ -20,22 +18,13 @@ public class CheckManager : MonoBehaviour
     [Tooltip("Set to 0 unless you need to test sometingand you know what you are doing.")]
     public int dashFragCount = 0;
     // Start is called before the first frame update
-    [DllImport("Oniris_Randomizer")]
-    private static extern void Randomizer(int[] p_content, int size);
     void Start()
     {
         assignedStatus = new bool[content.Length];
-        
-        int[] convertedArray = System.Array.ConvertAll(content, value => (int)value);
-        foreach (int item in convertedArray)
-        {
-            Debug.Log(item);
-        }
-        Randomizer(convertedArray, convertedArray.Length);
-        foreach (int item in convertedArray)
-        {
-            Debug.Log(item);
-        }
+        //for(int i = 0; i < content.Length; i++)
+        //{
+        //    assignedStatus[i] = false;
+        //}
     }
 
     // Update is called once per frame
