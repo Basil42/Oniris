3a07ee534624a89e6bf362e352a47bb88f87ce28
vororo <adamolsson@outlook.com>
2019-05-08 17:01:11 +0200

    Made a moving platform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    public float speed;
+    public int directionX;
+    public int directionY;
+    public int directionZ;
+    private Vector3 targetPosition;
+
+    public float loopTime;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        targetPosition = new Vector3(directionX, directionY, directionZ);
+    }
+
+    private void FixedUpdate()
+    {
+        timer += Time.fixedDeltaTime;
+        if(timer >= loopTime)
+        {
+            targetPosition *= -1;
+            timer = 0;
+        }
+        rb.MovePosition(transform.position + targetPosition * speed * Time.fixedDeltaTime);
+    }
+}
