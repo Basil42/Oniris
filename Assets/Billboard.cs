d4d44ef021e55fdd1a6d7ab9f140c7186f4e29ba
Isak Mansén <roostatbrood@gmail.com>
2019-05-08 10:30:32 +0200

    Fixed billboarding script for candlelight

---
+++ Assets/Billboard.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour
+{
+    
+
+    //Orient the camera after all movement is completed this frame to avoid jittering
+    void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform.position,Vector3.up);
+    }
+}
