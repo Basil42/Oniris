83c636130009d089339b45e6b8587919bbea1d05
Basil Gerard <nicolas1gerard@hotmail.fr>
2019-05-30 13:24:07 +0200

    audio stuff

---
+++ Assets/Scripts/musicManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class musicManager : MonoBehaviour
+{
+    public AudioMixerGroup Piano;
+    public AudioMixerGroup Chorus;
+    public AudioMixerGroup Flute;
+    public AudioMixerGroup Synth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
