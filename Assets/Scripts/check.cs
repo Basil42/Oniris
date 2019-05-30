c769852151ecd16eb61b76d007f14ad61d7ccbf9
Basil Gerard <nicolas1gerard@hotmail.fr>
2019-05-30 17:21:38 +0200

    Music base implementation

--- Assets/Scripts/check.cs
+++ Assets/Scripts/check.cs
@@ -15,6 +15,7 @@ public class check : MonoBehaviour
     private CheckManager m_manager;
     private VisualEffect m_passive;
     private VisualEffect m_pickup;
+    private musicManager m_musicManager;
     [SerializeField] private float dialogueTime = 10;
     // Start is called before the first frame update
     void Awake()
@@ -22,6 +23,7 @@ public class check : MonoBehaviour
         //if (index == -1) Debug.LogError("cannot have check without proper index");
         //m_manager = GameObject.FindGameObjectWithTag("checkManager").GetComponent<CheckManager>();
         //m_content = m_manager.getContent(index);
+        m_musicManager = GameObject.FindGameObjectWithTag("MusicManager").GetComponent<musicManager>();
     }
     private void Start()
     {
@@ -45,16 +47,19 @@ public class check : MonoBehaviour
                     player.m_abilityFlags |= AbilityAvailability.hasDoublejump;
                     CallDialogue("I think I got a new power! Press A in the air to double jump!");
                     GameObject.FindGameObjectWithTag("DoubleJumpUI").GetComponent<Image>().fillAmount = 1;
+                    m_musicManager.StartCoroutine(m_musicManager.ChoirFadeIn());
                     break;
                 case checkContent.Blink:
                     player.m_abilityFlags |= AbilityAvailability.hasBlink;
                     CallDialogue("I think I got a new power! Press Y to blink! I might be able to go through certain walls with this");
                     GameObject.FindGameObjectWithTag("BlinkUI").GetComponent<Image>().fillAmount = 1;
+                    m_musicManager.StartCoroutine(m_musicManager.SynthFadeIn());
                     break;
                 case checkContent.Wall_Jump:
                     player.m_abilityFlags |= AbilityAvailability.hasWallJump;
                     CallDialogue("I think I got a new power! Hold RT to wall run, horizontally and vertically, press A to jump during wall run!");
                     GameObject.FindGameObjectWithTag("WallJumpUI").GetComponent<Image>().fillAmount = 1;
+                    m_musicManager.StartCoroutine(m_musicManager.FluteFadeIn());
                     break;
                 default:
                     Debug.LogError("Check has no content, ensure that its index is valid.");
--- Assets/Scripts/musicManager.cs
+++ Assets/Scripts/musicManager.cs
@@ -4,19 +4,80 @@ using UnityEngine;
 using UnityEngine.Audio;
 public class musicManager : MonoBehaviour
 {
-    public AudioMixerGroup Piano;
-    public AudioMixerGroup Chorus;
-    public AudioMixerGroup Flute;
-    public AudioMixerGroup Synth;
-    // Start is called before the first frame update
+    public AudioMixer mixer;
+    private PlayerMovement m_player;
+    private float m_MasterFadeSpeed = 0.2f;
+    private float m_MasterVolume;
+    public float m_targetMasterVolume;
+    private float m_SynthVolume;
+    private float m_targetSynthVolume;
+    private float m_FluteVolume;
+    private float m_targetFluteVolume;
+    private float m_ChoirVolume;
+    private float m_targetChoirVolume;
+    private float m_SynthFadeSpeed = 0.05f;
+    private float m_ChoirFadeSpeed = 0.05f;
+    private float m_FluteFadeSpeed = 0.05f;
+
     void Start()
     {
-        
+        mixer.GetFloat("MasterVol", out m_targetMasterVolume);
+        mixer.GetFloat("SynthVol", out m_targetSynthVolume);
+        mixer.GetFloat("FluteVol", out m_targetFluteVolume);
+        mixer.GetFloat("ChoirVol", out m_targetChoirVolume);
+        m_player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        mixer.SetFloat("MasterVol", -80.0f);
+        mixer.SetFloat("SynthVol", -80.0f);
+        mixer.SetFloat("FluteVol", -80.0f);
+        mixer.SetFloat("ChoirVol", -80.0f);
+        mixer.GetFloat("MasterVol", out m_MasterVolume);
+        mixer.GetFloat("SynthVol", out m_SynthVolume);
+        mixer.GetFloat("FluteVol", out m_FluteVolume);
+        mixer.GetFloat("ChoirVol", out m_ChoirVolume);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        mixer.GetFloat("MasterVol", out m_MasterVolume);
+        mixer.SetFloat("MasterVol", Mathf.MoveTowards(m_MasterVolume, (Mathf.Clamp01(m_player.MovementVector.magnitude / m_player.m_RunningSpeed) * 40.0f) - 40.0f, m_MasterFadeSpeed));
+
+    }
+
+    public IEnumerator SynthFadeIn()
+    {
+        float lerpStep = 0.0f;
+        while(!Mathf.Approximately(m_SynthVolume, m_targetSynthVolume))
+        {
+            
+            mixer.SetFloat("SynthVol", Mathf.Lerp(-80.0f, m_targetSynthVolume, lerpStep));
+            lerpStep += m_SynthFadeSpeed;
+            yield return new WaitForFixedUpdate();
+            mixer.GetFloat("SynthVol", out m_SynthVolume);
+        }
+    }
+
+    public IEnumerator FluteFadeIn()
+    {
+        float lerpStep = 0.0f;
+        while (!Mathf.Approximately(m_FluteVolume, m_targetFluteVolume))
+        {
+            mixer.SetFloat("FluteVol", Mathf.Lerp(-80.0f, m_targetFluteVolume, lerpStep));
+            lerpStep += m_FluteFadeSpeed;
+            yield return new WaitForFixedUpdate();
+            mixer.GetFloat("FluteVol", out m_FluteVolume);
+        }
+    }
+
+    public IEnumerator ChoirFadeIn()
+    {
+        float lerpStep = 0.0f;
+        while (!Mathf.Approximately(m_ChoirVolume, m_targetChoirVolume))
+        {
+            mixer.SetFloat("ChoirVol", Mathf.Lerp(-80.0f, m_targetChoirVolume, lerpStep));
+            lerpStep += m_ChoirFadeSpeed;
+            yield return new WaitForFixedUpdate();
+            mixer.GetFloat("ChoirVol", out m_ChoirVolume);
+        }
     }
 }
