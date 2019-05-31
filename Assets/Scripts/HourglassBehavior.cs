64ac699c2e451fd19d9dab7009165cd015dc446f
Basil Gerard <nicolas1gerard@hotmail.fr>
2019-05-31 12:57:52 +0200

    Hourglass visual done

--- Assets/Scripts/HourglassBehavior.cs
+++ Assets/Scripts/HourglassBehavior.cs
@@ -7,6 +7,8 @@ public class HourglassBehavior : MonoBehaviour
     // Start is called before the first frame update
     GameObject Hourglass;
     public float RotationSpeed =  0.1f;
+    public float timer = 5.0f;
+    private float internalTime = 0.0f;
     private void Awake()
     {
         Hourglass = transform.GetChild(1).gameObject;
@@ -19,7 +21,7 @@ public class HourglassBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        internalTime += Time.deltaTime;
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -28,6 +30,7 @@ public class HourglassBehavior : MonoBehaviour
 
     IEnumerator RotateHourglass(int angle)
     {
+        internalTime = 0.0f;
         float step = 0.0f;
         float smoothstep = 0.0f;
         float StartingAngle = Hourglass.transform.localRotation.eulerAngles.z;
@@ -39,7 +42,15 @@ public class HourglassBehavior : MonoBehaviour
             Hourglass.transform.localRotation = Quaternion.Lerp(StartingRotation, TargetRotation, smoothstep);
             step += RotationSpeed * Time.deltaTime;
             smoothstep = Mathf.SmoothStep(0.0f, 1.0f, step);
+            Hourglass.GetComponent<Renderer>().material.SetFloat("_GoldGradient", Mathf.Clamp(smoothstep,0.0f,1.0f));
             yield return null;
         }
+        while(internalTime < timer)
+        {
+            Hourglass.GetComponent<Renderer>().material.SetFloat("_GoldGradient", Mathf.Clamp(-Mathf.Clamp01(internalTime/timer)+1.0f, 0.0f, 1.0f));
+            
+            yield return null;
+        }
+        
     }
 }
