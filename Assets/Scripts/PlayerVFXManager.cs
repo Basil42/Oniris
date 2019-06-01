7ba80674a7011a7adcbe6edbcb5b76d8f6a58679
Basil Gerard <nicolas1gerard@hotmail.fr>
2019-06-01 19:00:14 +0200

    Success

--- Assets/Scripts/PlayerVFXManager.cs
+++ Assets/Scripts/PlayerVFXManager.cs
@@ -7,8 +7,10 @@ public class PlayerVFXManager : MonoBehaviour
 {
     public GameObject RunEffectObject;
     public GameObject BlinkEffectObject;
+    public Renderer HairRenderer;
     private VisualEffect RunEffect;
     private VisualEffect BlinkEffect;
+    private PlayerMovement playerMovement;
     private Vector3 oldPosition;
     private Vector3 deltaPosition;
     private Vector3 SpeedEffectoffset;
@@ -16,13 +18,14 @@ public class PlayerVFXManager : MonoBehaviour
     [SerializeField] float SpeedTreshold = 0.05f;
     [SerializeField] int spawnRate = 10;
     //VisualEffect RunEffect2;
+    private float m_windPrevious = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
         RunEffect = RunEffectObject.GetComponent<VisualEffect>();
         BlinkEffect = BlinkEffectObject.GetComponent<VisualEffect>();
         SpeedEffectoffset = RunEffect.transform.localPosition;
-        
+        playerMovement = GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
@@ -40,6 +43,14 @@ public class PlayerVFXManager : MonoBehaviour
         }
         BlinkEffectObject.transform.position = transform.position;
         BlinkEffect.SetVector3("attractive target position", transform.position + BlinkEffectTargetOffset);
+        HairRenderer.material.SetFloat("Vector1_7F45795D", HairRenderer.material.GetFloat("Vector1_7F45795D") + Time.deltaTime * (1.0f +m_windPrevious * 5.0f));
+    }
+    private void FixedUpdate()
+    {
+        m_windPrevious = HairRenderer.material.GetFloat("Vector1_F38142DE");
+        
+        HairRenderer.material.SetFloat("Vector1_F38142DE", Mathf.Lerp(m_windPrevious, Mathf.Clamp01(Vector3.Project(playerMovement.MovementVector, transform.forward).magnitude / playerMovement.m_RunningSpeed),0.05f ));
+        
     }
 
     private void RunUpdate()
