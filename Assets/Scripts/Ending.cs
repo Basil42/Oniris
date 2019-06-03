e65a8c72a641848dd1c164347720749882de0139
Basil <nicolas1gerard@hotmail.fr>
2019-06-04 01:01:13 +0200

    ending

---
+++ Assets/Scripts/Ending.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour
+{
+    public RawImage WhiteImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WhiteImage.CrossFadeAlpha(0.0f, 3.0f, true);
+        StartCoroutine(CreditRoll());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator CreditRoll()
+    {
+        AsyncOperation load = SceneManager.LoadSceneAsync("Credits");
+        load.allowSceneActivation = false;
+        yield return new WaitUntil(() => Input.anyKey);
+        load.allowSceneActivation = true;
+    }
+}
